76dbd56e1983852c40728fbda6c338c401bc6aab
Rai Osborne <rai.osborne@audacia.co.uk>
2022-08-22 14:16:25 +0100

    refactor: commented out currently unused classes

--- Social/class-messages.ts
+++ Social/class-messages.ts
@@ -1,26 +1,26 @@
-interface iMessages{
-    //properties
-    id:number;
-    message:any;
-    userId:number;
-    chatId:number;
+// interface iMessages{
+//     //properties
+//     id:number;
+//     message:any;
+//     userId:number;
+//     chatId:number;
 
-    //methods
-    //setUp(id:number, message:any, userId:number, chatId:number);
-}
+//     //methods
+//     //setUp(id:number, message:any, userId:number, chatId:number);
+// }
 
-class Messages implements iMessages{
-    //properties
-    id:number;;
-    message:any;
-    userId:number;
-    chatId:number;
+// class Messages implements iMessages{
+//     //properties
+//     id:number;;
+//     message:any;
+//     userId:number;
+//     chatId:number;
 
-    //methods
-    constructor(id:number, message:any, userId:number, chatId:number){
-        this.id = id;
-        this.message = message;
-        this.userId = userId;
-        this.chatId = chatId;
-    }
-}
+//     //methods
+//     constructor(id:number, message:any, userId:number, chatId:number){
+//         this.id = id;
+//         this.message = message;
+//         this.userId = userId;
+//         this.chatId = chatId;
+//     }
+// }
--- Social/class-photos.ts
+++ Social/class-photos.ts
@@ -1,29 +1,29 @@
-interface iPhotos{
-    //properties
-    id:number;
-    dateUploaded:Date;
-    url:any;
-    userId:number;
-    postId:number;
+// interface iPhotos{
+//     //properties
+//     id:number;
+//     dateUploaded:Date;
+//     url:any;
+//     userId:number;
+//     postId:number;
 
-    //methods
-   // setUp(id:number, dateUploaded:Date, url:any, userId:number, postId:number);
-}
+//     //methods
+//    // setUp(id:number, dateUploaded:Date, url:any, userId:number, postId:number);
+// }
 
-class Photos implements iPhotos{
-    //properties
-    id:number;
-    dateUploaded:Date;
-    url:any;
-    userId:number;
-    postId:number;
+// class Photos implements iPhotos{
+//     //properties
+//     id:number;
+//     dateUploaded:Date;
+//     url:any;
+//     userId:number;
+//     postId:number;
 
-    //methods
-    constructor(id:number, dateUploaded:Date, url:any, userId:number, postId:number){
-        this.id = id;
-        this.dateUploaded = dateUploaded;
-        this.url = url
-        this.userId = userId;
-        this.postId = postId;
-    }
-}
+//     //methods
+//     constructor(id:number, dateUploaded:Date, url:any, userId:number, postId:number){
+//         this.id = id;
+//         this.dateUploaded = dateUploaded;
+//         this.url = url
+//         this.userId = userId;
+//         this.postId = postId;
+//     }
+// }
